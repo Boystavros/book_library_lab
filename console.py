1d2169443b67faf092f5b30894906c793d65f7c0
Sacha Ponniah <sponniah812@gmail.com>
2021-12-01 14:51:23 +0000

    Initial commit. Creates file structure.

---
+++ console.py
@@ -0,0 +1,7 @@
+import pdb
+from models.author import Author
+from models.book import Book
+
+import repositories.book_repository as book_repository
+import repositories.author_repository as author_repository
+
